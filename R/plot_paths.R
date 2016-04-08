965bc552891af0d007a5fcdcbe7d03c78ad2463f
topolskib <bartek.topolski@gmail.com>
2016-04-08 23:59:23 +0200

    Signed-off-by: topolskib <bartek.topolski@gmail.com>

--- R/plot_paths.R
+++ R/plot_paths.R
@@ -7,5 +7,5 @@ plot_paths <- function(data, slownik, alpha_mod = 1){
   pocz <- slownik[data$from, 4:5]
   kon <- slownik[data$to, 4:5]
   print(head(pocz))
-  plot_polaczenia(pocz, kon, alpha = alpha*alpha_mod)
+  plot_polaczenia(pocz, kon, alpha = alpha^alpha_mod)
 }
