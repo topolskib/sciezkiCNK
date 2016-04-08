9f42efe6d212ba6abd1e380e46c58d101b941bee
topolskib <bartek.topolski@gmail.com>
2016-04-08 22:57:34 +0200

    Signed-off-by: topolskib <bartek.topolski@gmail.com>

--- R/filter_data.R
+++ R/filter_data.R
@@ -4,6 +4,7 @@
 filter_data <- function(weekdays = c(1,7), months = c(1,12), hours=c(9,20)) {  
   require(dplyr)
   require(stringi)
+  daty <- seq.Date(from = as.Date("2013-01-01"), to = as.Date("2013-12-31"), by = 1)
   days <- format(daty[as.numeric(format.Date(daty, format ="%u")) %in% weekdays], format = "%d_%m")
   filtered <- dane_all[days]
   filtered <- filtered[(as.numeric(stri_sub(names(filtered), 4,5)) %in% months)]
